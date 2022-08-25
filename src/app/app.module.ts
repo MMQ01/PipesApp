971d08d879d0715145d9883fa8bbbb89039de138
Marcelo <marcelomorales2001@gmail.com>
2022-08-24 21:20:04 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRouterModule,
+    BrowserModule,
+    PrimeNgModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
